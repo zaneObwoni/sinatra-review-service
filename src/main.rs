d58dbf470dd78b881a7ed5308b1dae44c21557ab
rafiki <zaneobwoni@gmail.com>
2023-02-11 04:46:23 +0100

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+fn new_routes() -> Router {
+    Router::new().route("/", get(|| async { "Hello, World!" }))
+}
+
+#[tokio::main]
+async fn main() {
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let app = Router::new().nest("/api", new_routes());
+
+    println!("Listening on http://[::1]:8000");
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .expect("Server failed to run!");
 }
